c53ea68b750bbcc95543cbfb0f71504b31635c48
Naman Modi <modi.naman14@gmail.com>
2019-06-04 12:17:54 +0530

    added footer

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="pv4 ph3 ph5-m ph6-l mid-gray">
+            <small class="f6 db tc">© 2019 <b class="ttu">Naman Modi</b>., All Rights Reserved</small>
+            <div class="tc mt3">
+                <a href="https://github.com/imnamanmodi1" title="Language" class="f6 dib ph2 link mid-gray dim">Github Profile</a>
+                <a href="https://digitaliz.in" title="Terms" class="f6 dib ph2 link mid-gray dim">Company Site</a>
+                <a href="#" title="Privacy" class="f6 dib ph2 link mid-gray dim">My Blog</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import './styles/themeColors.scss';
 import Game from './game';
-import Header from './header'
+import Header from './header';
+import Footer from './footer';
 
 ReactDOM.render(
     <div>
         <Header />
         <Game />
+        <Footer />
     </div>
 , document.getElementById("root"));
